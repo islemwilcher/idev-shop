caf73eb0401df8f2b984ce6a26b1b0d4d442d01a
islemwilcher <islemadobe6@gmail.com>
2021-11-28 12:42:49 +0100

    adding the auth and category in the api

--- src/api/index.js
+++ src/api/index.js
@@ -11,9 +11,21 @@ API.interceptors.request.use((req) => {
     return req
 })
 
+//auth
+export const signIn = (formData) => API.post('/auth/signin', formData)
+export const signUp = (dormData) => API.post('/auth/signup', dormData)
+
+//products
 export const fetchProducts = () => API.get('/products')
 export const fetchProduct = (id) => API.get(`/products/find/${id}`)
 export const createProduct = (newProduct) => API.post('/products', newProduct)
 export const updateProduct = (id, updatedProduct) => API.patch(`/products/${id}`, updatedProduct)
 export const deleteProduct = (id) => API.delete(`/products/${id}`)
+
+//categories
+export const fetchCategories = () => API.get('/categories')
+export const fetchCategory = (id) => API.get(`/categories/find/${id}`)
+export const createCategory = (newCategory) => API.post('/categories', newCategory)
+export const updateCategory = (id, updatedCategory) => API.patch(`/categories/${id}`, updatedCategory)
+export const deleteCategory = (id) => API.delete(`/categories/${id}`)
 
