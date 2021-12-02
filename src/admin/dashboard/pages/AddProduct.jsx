a840d79d52eb3ac3ec1b0200ada03ad5798dbdc9
islemwilcher <islemadobe6@gmail.com>
2021-12-02 13:56:02 +0100

    adding the dashboard component, adding the dashboard  and add product page

---
+++ src/admin/dashboard/pages/AddProduct.jsx
@@ -0,0 +1,15 @@
+
+import styled from 'styled-components'
+
+const Contaner = styled.div``
+
+const AddProduct = () => {
+    return (
+        <Contaner>
+
+            
+        </Contaner>
+    )
+}
+
+export default AddProduct
