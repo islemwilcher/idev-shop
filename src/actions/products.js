46442d7c3bde34429e108b89104350d3ffc4e8a4
islemwilcher <islemadobe6@gmail.com>
2022-02-26 21:36:13 +0100

    get off, the extra actions

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,5 +1,5 @@
 
-import { START_LOADING, END_LOADING, FETCH_ALL, FETCH_ALL_NEW, FETCH_NEW, FETCH_PRODUCT, CREATE, UPDATE, DELETE, FETCH_ALL_BEST, FETCH_BEST } from '../constants/actiontype'
+import { START_LOADING, END_LOADING, FETCH_ALL, FETCH_PRODUCT, CREATE, UPDATE, DELETE } from '../constants/actiontype'
 import * as api from '../api/index.js'
 
 //all products
@@ -60,49 +60,5 @@ export const deleteProduct = (id) => async (dispatch) => {
         dispatch({ type: DELETE, payload: id });
     } catch (error) {
         console.log(error);
-    }
-}
-
-//all new products
-export const allNEWProducts = () => async (dispatch) => {
-    try {
-        const { data } = await api.allNEWProducts()
-
-        dispatch({ type: FETCH_ALL_NEW, payload: data })
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-//new products
-export const neWProducts = () => async (dispatch) => {
-    try {
-        const { data } = await api.neWProducts()
-
-        dispatch({ type: FETCH_NEW, payload: data })
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-//all best products
-export const allBESTProducts = () => async (dispatch) => {
-    try {
-        const { data } = await api.allBESTProducts()
-
-        dispatch({ type: FETCH_ALL_BEST, payload: data })
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-//best products
-export const besTProducts = () => async (dispatch) => {
-    try {
-        const { data } = await api.besTProducts()
-
-        dispatch({ type: FETCH_BEST, payload: data })
-    } catch (error) {
-        console.log(error)
     }
 }
