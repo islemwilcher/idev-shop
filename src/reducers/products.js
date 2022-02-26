94f32ba0253ec9fff8333f848093c1b133d18fb5
islemwilcher <islemadobe6@gmail.com>
2022-02-26 21:12:48 +0100

    get of all the extra functionalities such as new and all new , best and all best

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,6 +1,6 @@
-import { FETCH_ALL, FETCH_ALL_NEW, FETCH_NEW,FETCH_ALL_BEST, FETCH_BEST, FETCH_PRODUCT, CREATE, UPDATE, DELETE, START_LOADING, END_LOADING } from '../constants/actiontype';
+import { FETCH_ALL, FETCH_PRODUCT, CREATE, UPDATE, DELETE, START_LOADING, END_LOADING } from '../constants/actiontype';
 
-const productsReducer = (state = { isLoading: true, products: [], newProducts: [], allNewProducts: [], bestProducts: [], allBestProducts: [],} , action) => {
+const productsReducer = (state = { isLoading: true, products: [] } , action) => {
   switch (action.type) {
     case START_LOADING:
       return { ...state, isLoading: true };
@@ -8,14 +8,6 @@ const productsReducer = (state = { isLoading: true, products: [], newProducts: [
       return { ...state, isLoading: false };
     case FETCH_ALL:
       return {...state, products: action.payload };
-    case FETCH_ALL_NEW:
-      return {...state, allNewProducts: action.payload };
-    case FETCH_NEW:
-      return {...state, newProducts: action.payload };
-    case FETCH_ALL_BEST:
-      return {...state, allBestProducts: action.payload };
-    case FETCH_BEST:
-      return {...state, bestProducts: action.payload };
     case FETCH_PRODUCT:
       return { ...state, product: action.payload.product };
     case CREATE:
