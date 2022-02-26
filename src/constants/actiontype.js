a2332d1c59d9c7270a2bcd8bfc9d06219c779872
islemwilcher <islemadobe6@gmail.com>
2022-02-26 21:30:45 +0100

    minimising the actions

--- src/constants/actiontype.js
+++ src/constants/actiontype.js
@@ -3,33 +3,26 @@
 export const AUTH = 'AUTH';
 export const LOGOUT = 'LOGOUT';
 
+//LOADING
+export const END_LOADING = 'END_LOADING';
+export const START_LOADING = 'START_LOADING';
+
+//CRUD
+export const FETCH_ALL = 'FETCH_ALL';
 export const CREATE = 'CREATE';
 export const UPDATE = 'UPDATE';
 export const DELETE = 'DELETE';
 
-
 //CATEGORIES
-export const FETCH_ALL_CAREGORIES = 'FETCH_ALL_CAREGORIES';
 export const FETCH_CATEGORY = 'FETCH_CATEGORY';
+
+//PRODUCTS
+export const FETCH_PRODUCT = 'FETCH_PRODUCT';
+
+//ORDERS
+export const FETCH_ORDER = 'FETCH_ORDER';
 
 //CARTS
 export const ADD_TO_CART= 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 export const CART_RESET = 'CART_RESET';
-
-//PRODUCTS
-export const FETCH_ALL = 'FETCH_ALL';
-export const FETCH_PRODUCT = 'FETCH_PRODUCT';
-
-export const FETCH_NEW = 'FETCH_NEW';
-export const FETCH_ALL_NEW = 'FETCH_ALL_NEW';
-
-export const FETCH_BEST = 'FETCH_BEST';
-export const FETCH_ALL_BEST = 'FETCH_ALL_BEST';
-
-export const END_LOADING = 'END_LOADING';
-export const START_LOADING = 'START_LOADING';
-
-//ORDERS
-export const FETCH_ALL_ORDERS = 'FETCH_ALL_ORDERS';
-export const FETCH_ORDER = 'FETCH_ORDER';
